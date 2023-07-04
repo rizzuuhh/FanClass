7067bfc42fee386ea0c4592bcef8033466299a0d
rizza <denostarizza43@gmail.com>
2023-07-05 02:19:57 +1200

    setter method for radius added

--- fanclass.py
+++ fanclass.py
@@ -9,6 +9,10 @@ class Fan:
     def set_speed(self, speed):
         self.__speed = speed
 
+    def set_radius(self, radius):
+        self.__radius = radius
+
+
 # Getter methods
 # Create the first Fan object
 # Create the second Fan object
