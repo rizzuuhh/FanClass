c803f8f5d115aa70d17d4dc19b74670b61661270
rizza <denostarizza43@gmail.com>
2023-07-05 02:05:50 +1200

    Define the Fan Class

--- fanclass.py
+++ fanclass.py
@@ -1,4 +1,10 @@
 # Define the Fan class with the specified attributes
+class Fan:
+    def __init__(self):
+        self.__speed = "SLOW"
+        self.__radius = 5
+        self.__color = "blue"
+        self.__on = False
 # Setter methods
 # Getter methods
 # Create the first Fan object
