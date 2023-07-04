df4ed5db88e1db6a80416d57b4a554071e861304
rizza <denostarizza43@gmail.com>
2023-07-05 01:52:43 +1200

    Pseudocode

---
+++ fanclass.py
@@ -0,0 +1,7 @@
+# Define the Fan class with the specified attributes
+# Setter methods
+# Getter methods
+# Create the first Fan object
+# Create the second Fan object
+# Display each object's properties
+
